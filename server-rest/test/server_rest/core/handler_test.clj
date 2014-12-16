1c33cabb0702037a80176a3421829bab3ae9db8c
King, Cathal(e19800) <Cathal.King@cmegroup.com>
2014-12-16 14:59:18 +0000

    Added simple desciptive unit test for json-response helper method. Removed usage of raw escaped json strings in the tests for readability. Need to merge in Tim M's in-mem persistence changes also.

--- server-rest/test/server_rest/core/handler_test.clj
+++ server-rest/test/server_rest/core/handler_test.clj
@@ -1,14 +1,34 @@
 (ns server-rest.core.handler-test
   (:use midje.sweet)
   (:require [ring.mock.request :as mock]
-            [server-rest.core.handler :as handler]))
+            [server-rest.core.handler :as handler]
+            [server-rest.core.handler :as handler]
+            [cheshire.core :as cheshire]))
+
+(defn to-json [data]
+  (cheshire/generate-string data))
+
+(fact "json-response returns map containing 
+      :status value
+      :body with some-data as json string
+      :header as map with Content-Type=application/json, Access-Control-Allow-Origin=*"
+  (let [a-status 201
+        some-data {:name "blah"}]
+    (:status (handler/json-response a-status some-data)) => 201
+    (:headers (handler/json-response a-status some-data)) => {"Content-Type" "application/json", "Access-Control-Allow-Origin", "*"}
+    (:body (handler/json-response a-status some-data)) => (to-json some-data)))
 
 (fact "List all leagues, then return empty list"
   (let [response (handler/app (mock/request :get "/league"))]
     (:status response) => 200
-    (:body response) => "[]"))
+    (:body response) => (to-json [])))
 
 (fact "Build a league, given a name and description"
-  (let [response (handler/app (mock/request :put "/league" "{\"name\": \"pongpong\",\"description\":\"Only Tim Knows\"}"))]
+  (let [response (handler/app (mock/request :put "/league" (to-json {:name "pongpong" :description "Only Tim Knows"})))]
     (:status response) => 201
-    (:body response) => "{\"name\":\"pongpong\",\"description\":\"Only Tim Knows\"}"))
+    (:body response) => (to-json {:name "pongpong" :description "Only Tim Knows"})))
+
+(fact "Get a league, given a name"
+      (let [response (handler/app (mock/request :get "/league/pongpong"))]
+        (:status response) => 201
+        (:body response) => (to-json {:name "pongpong" :description "Only Tim Knows"})))
